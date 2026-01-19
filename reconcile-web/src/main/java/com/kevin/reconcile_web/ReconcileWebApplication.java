cbdac0929f6ff24ea7f1043c179c11b4767d757b
IAM-soo <phkevin.soo@gmail.com>
2026-01-19 14:28:40 +0900

    main feature

---
+++ reconcile-web/src/main/java/com/kevin/reconcile_web/ReconcileWebApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.reconcile_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconcileWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconcileWebApplication.class, args);
+	}
+
+}
---
+++ reconcile-web/src/test/java/com/kevin/reconcile_web/ReconcileWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.reconcile_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconcileWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
